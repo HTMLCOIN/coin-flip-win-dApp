0473e9bac84828f25141c5b1f9ea0212f1ee5e34
Samoila Gabor <samoila.gabor@iquestgroup.com>
2020-12-04 10:06:34 +0200

    update UI, add makeBet, withdraw and withdrawAll functionality

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
+let coinOption;
+let isHeadSelected;
+const betInput = document.getElementById('betAmountInput');
+betInput.setAttribute('disabled', 'true');
+
+function showGenre(item) {
+  betInput.removeAttribute('disabled');
+  coinOption = item.textContent.trim();
+  document.getElementById("coinOption").innerHTML = item.innerHTML;
+}
+
 const web3 = new Web3(Web3.givenProvider);
+const CONTRACT_ADDRESS = '0x11180B9941b8b88Dda52C6FCEB283F7F1D6E2Ba8';
 let contractInstance;
 
 window.addEventListener('DOMContentLoaded', () => {
   window.ethereum.enable().then((accounts) => {
     console.log(accounts);
-    const addressCoinFlipContract = '0x22f3C81818cc3Ab27458a36B7Ea28D3b5F627030';
-    contractInstance = new web3.eth.Contract(abi, addressCoinFlipContract, { from: accounts[0] });
-    // getContractBalance();
+    contractInstance = new web3.eth.Contract(abi, CONTRACT_ADDRESS, { from: accounts[0] });
+    getContractBalance();
   })
   
-
   const head = document.getElementById("coin-head");
   const tail = document.getElementById("coin-tail");
   const coinSelection = document.getElementById("coin-selection");
+  selectHead(head, tail, coinSelection);
   head.addEventListener("click", () => selectHead(head, tail, coinSelection));
   tail.addEventListener("click", () => selectTail(head, tail, coinSelection));
 
-  document.getElementById('bet-button').addEventListener('click', makeBet);
-  document.getElementById('deposit-button').addEventListener('click', depositFunds);
-  document.getElementById('contract-balance-button').addEventListener('click', getContractBalance);
+  document.getElementById('makeBetButton').addEventListener('click', makeBet);
+  document.getElementById('depositButton').addEventListener('click', depositFunds);
   document.getElementById("withdraw-button").addEventListener('click', withdraw);
   document.getElementById("withdraw-all-button").addEventListener('click', withdrawAll);
 });
@@ -27,16 +37,20 @@ const selectHead = (head, tail, coinSelection) => {
   tail.classList.remove("selected");
   head.classList.add("selected");
   coinSelection.textContent = 'Head';
+  isHeadSelected = true;
 }
 
 const selectTail = (head, tail, coinSelection) => {
   head.classList.remove("selected");
   tail.classList.add("selected");
   coinSelection.textContent = 'Tail';
+  isHeadSelected = false;
 }
 
 const depositFunds = () => {
-  const depositAmount = (document.getElementById("deposit-amount").value).trim();
+  console.log('depositFunds');
+  const depositAmount = (document.getElementById("depositAmount").value).trim();
+  console.log('depositAmount', depositAmount);
   if (isNaN(depositAmount)) {
     return;
   }
@@ -45,8 +59,11 @@ const depositFunds = () => {
   }
   contractInstance.methods.depositFunds().send(config)
     .on('transactionHash', hash => console.log('Transaction hash: ', hash))
-    .on('confirmation', confNum => console.log('Conf. number: ', confNum))
-    .on('receipt', receipt => console.log('Receipt: ', receipt));
+    .on('confirmation', confNum => console.log('Confirmation number: ', confNum))
+    .on('receipt', receipt => {
+      getContractBalance();
+      console.log('Receipt: ', receipt)
+    });
 }
 
 const getContractBalance = () => {
@@ -63,26 +80,42 @@ const withdraw = () => {
     return;
   }
   const weiAmount = web3.utils.toWei(withdrawAmount, 'ether');
-  contractInstance.methods.withdraw(weiAmount).call()
-    .then(response => {
-      console.log('withdraw response: ', response);
-    })
+  contractInstance.methods.withdraw(weiAmount).send()
+    .on('transactionHash', hash => console.log('Transaction hash: ', hash))
+    .on('confirmation', confNum => console.log('Confirmation number: ', confNum))
+    .on('receipt', receipt => {
+      getContractBalance();
+      console.log('Receipt withdraw: ', receipt)
+    });
 }
 
 const withdrawAll = () => {
-  contractInstance.methods.withdrawAll().call()
-    .then(response => {
-      console.log('withdraw response: ', response);
-    })
+  contractInstance.methods.withdrawAll().send()
+    .on('transactionHash', hash => console.log('Transaction hash: ', hash))
+    .on('confirmation', confNum => console.log('Confirmation number: ', confNum))
+    .on('receipt', receipt => {
+      getContractBalance();
+      console.log('Receipt withdraw all: ', receipt)
+    });
 }
 
 
 const makeBet = () => {
-  const betInput = document.getElementById('bet-input');
-  const config = {
-    value: web3.utils.toWei('1', 'ether')
+  const betInput = document.getElementById('betAmountInput');
+  if (isNaN(betInput.value)) {
+    alert('Invalid bet amount.');
+    return;
   }
 
-  // contractInstance.methods.
-  console.log('make bet:', betInput.value, isNaN(betInput.value));
+  const coinSide = +isHeadSelected;
+  const config = {
+    value: web3.utils.toWei(betInput.value, coinOption)
+  }
+  contractInstance.methods.makeBet(coinSide).send(config)
+    .on('transactionHash', hash => console.log('Transaction hash: ', hash))
+    .on('confirmation', confNum => console.log('Conf. number: ', confNum))
+    .on('receipt', receipt => {
+      getContractBalance();
+      console.log('Receipt after bet: ', receipt)
+    });
 }
